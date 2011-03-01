b92cdc3b46f3b16a528e94bc87a6c54ae12ac27b
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-01 11:50:21 -0800

    pivotal10597219 Insert blessed books in related books container (top)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :find_covers
+  
+  protected
+  def find_covers
+    @covers = Book.where({:blessed => true}).limit(8)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def list_element_link(path, text)
+    content_tag :li do
+      content_tag :a, :href => path do
+        text
+      end
+    end
+  end
 end
