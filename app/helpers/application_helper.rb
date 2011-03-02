3e97109008ea35d0af1d7fdaf6b656afae9f27fa
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-02 11:27:39 -0800

    p10539699 Books have relevant Open Graph metadata

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
   def typekit_include_helper
     javascript_include_tag('http://use.typekit.com/idk3svd.js').concat(javascript_tag "try{ Typekit.load();}catch(e){}")
   end
+  
+  def fb_open_graph_metadata(config = {})
+    content_tag(:meta, nil, {:property => "og:title", :content => "Classicly"}) +
+    content_tag(:meta, nil, {:property => "og:type", :content => "website"}) +
+    content_tag(:meta, nil, {:property => "og:url", :content => config[:url] || "http://www.classicly.com"}) +
+    content_tag(:meta, nil, {:property => "og:image", :content => "http://www.classicly.com/images/logo.png"}) +
+    content_tag(:meta, nil, {:property => "og:site_name", :content => "Classicly"}) +
+    content_tag(:meta, nil, {:property => "fb:app_id", :content => "191005167590330"}) +
+    content_tag(:meta, nil, {:property => "og:description", :content => config[:description] || "Classicly gives you free books for your laptop,  Kindle, Nook, iPad, or iPhone. Just hit download! From Shakespeare to F. Scott Fitzgerald we have all the classics available with a click, browsable with beautiful covers, great descriptions, and hand-picked collections"})
+  end
 end
