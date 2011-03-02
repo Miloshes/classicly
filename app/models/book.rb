26373df4cf177c4f9f133ca9d6cb329298024ce7
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-02 14:59:34 -0800

    p10539549 Download options pulled from actual available formats. Fix in all downloadable formats

--- app/models/book.rb
+++ app/models/book.rb
@@ -21,7 +21,7 @@ class Book < ActiveRecord::Base
   end
 
   def all_downloadable_formats
-    self.download_formats.where({:download_status => 'downloaded'}).select(:format).collect(&:format)
+    self.download_formats.where({:download_status => 'downloaded'}).map(&:format)
   end
 
   def classicly_formats
