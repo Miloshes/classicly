7069019e4f4e5a81efbfd281a32fe7637a9703a2
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-03-03 20:21:17 +0100

    fixing "incomplete multibyte character" error on PostgreSQL

--- db/migrate/20110303160251_add_pretty_title_to_books.rb
+++ db/migrate/20110303160251_add_pretty_title_to_books.rb
@@ -2,6 +2,8 @@ class AddPrettyTitleToBooks < ActiveRecord::Migration
   def self.up
     add_column :books, :pretty_title, :string
     
+    converter = Iconv.new('UTF-8//IGNORE', 'UTF-8')
+    
     Book.all.each do |book|
       new_title       = book.title
       lowercase_title = book.title.downcase
@@ -14,7 +16,12 @@ class AddPrettyTitleToBooks < ActiveRecord::Migration
         new_title = "An " + book.title[0...-4]
       end
       
-      book.pretty_title = new_title[0,255]
+      # cut it to comply PostgreSQL limit
+      new_title = new_title[0,255]
+      # convert to fix "incomplete multibyte character" errors on PostgreSQL
+      new_title = converter.iconv(new_title)
+      
+      book.pretty_title = new_title
       book.save
     end
   end
