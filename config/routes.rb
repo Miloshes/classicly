b199a88620c1a637424ad39074470a9e7b19a7a7
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-07 14:10:58 -0800

    p10759757 Added new route for downloads. Should point to author/book/download

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Classicly::Application.routes.draw do
   match "/:id" => "seo#show", :as => 'seo', :via => :get
   match "/:author_id/:id" => "books#show", :as => :author_book, :via => :get
-  match "/books/:id/download" => "books#download", :as => 'download_book', :via => :post
+  match "/:author_id/:id/download" => "books#download", :as => 'download_book', :via => :post
   root :to => 'pages#main'
 end
