e4e0b6941064c14b5cbabdd0a6ef48003552a46d
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-08 14:26:41 -0800

    p10876313 Install sitemap_generator. Generate site map

---
+++ config/sitemap.rb
@@ -0,0 +1,35 @@
+# Set the host name for URL creation
+SitemapGenerator::Sitemap.default_host = "http://www.classicly.com"
+
+SitemapGenerator::Sitemap.add_links do |sitemap|
+  # Put links creation logic here.
+  #
+  # The root path '/' and sitemap index file are added automatically.
+  # Links are added to the Sitemap in the order they are specified.
+  #
+  # Usage: sitemap.add(path, options={})
+  #        (default options are used if you don't specify)
+  #
+  # Defaults: :priority => 0.5, :changefreq => 'weekly',
+  #           :lastmod => Time.now, :host => default_host
+  # 
+  # 
+  # Examples:
+  # 
+  # Add '/articles'
+  #   
+  #   sitemap.add articles_path, :priority => 0.7, :changefreq => 'daily'
+  #
+  # Add individual articles:
+  #
+  #   Article.find_each do |article|
+  #     sitemap.add article_path(article), :lastmod => article.updated_at
+  #   end
+  Book.find_each do|book|
+    sitemap.add author_book_path(book.author, book) unless book.author.nil?
+  end
+
+  Collection.find_each do|collection|
+    sitemap.add seo_path(collection)
+  end
+end
