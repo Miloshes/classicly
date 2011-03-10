d5a750fef78854d3d6318afe9d8f983ca6e62c45
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-10 07:58:55 -0800

    p10842461 added metadata descriptions for collections

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -76,4 +76,14 @@ class Collection < ActiveRecord::Base
   def has_author_portrait?
     !self.author_portrait_updated_at.blank?
   end
+
+  def description_for_open_graph
+    case self.collection_type
+    when 'collection'
+      "%s- the ultimate literature collection. Dozens of hand-picked books for free download as PDF, Kindle, Sony Reader, iBooks, and more. You can also read online!" % self.name
+    when 'author'
+      "The world’s greatest collection of books by %s. Download free books, read online, or check out %s quotes and a hand-picked collection of featured titles." % ([self.name] * 2)
+    end
+  end
+
 end
