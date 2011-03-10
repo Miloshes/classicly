161c22e4b6fc11caf91fffd7861c1bd8aea2be76
Ignacio De La Madrid <ignacio.delamadrid@crowdint.com>
2011-03-09 19:35:20 -0800

    p10539811 Added author name to the query conditions to fetch books

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,10 @@ class SearchController < ApplicationController
 
   def show
     @search = params[:term]
-    @books = Book.joins(:collections).where({:title.matches => "%#{@search}%"} | {:collections => {:name.matches => "%#{@search}%"}}).page(params[:page]).per(10)
+    @books = Book.joins(:collections).joins(:author).where(
+      {:title.matches => "%#{@search}%"} | 
+      {:collections => {:name.matches => "%#{@search}%"}} |
+      {:author => {:name.matches => "%#{@search}%"}}).page(params[:page]).per(10)
   end
 
 end
