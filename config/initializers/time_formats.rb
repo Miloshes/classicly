de06b2e8c027bb0cb19cc9b2e5d8e77c135c7e61
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-03-28 15:37:29 -0700

    p11434629 css sprite for ratings fixed on hover. Padding for review textarea.

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,3 @@
-Time::DATE_FORMATS[:review_format] = "%B %d, %Y , %l:%M %P"
+Time::DATE_FORMATS[:review_format] = "%B %d, %Y, %l:%M %P"
 Time::DATE_FORMATS[:month_and_year] = "%B %Y"
 Time::DATE_FORMATS[:human] = '%m/%d/%y @ %I:%M%p'
