7faaab4d0286f1e7b03a75a3781fa3f3b968cce4
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-03-28 10:30:47 -0700

    p11393413 login id migration is not needed

--- db/migrate/20110322194030_add_login_id_to_reviews.rb
+++
@@ -1,9 +0,0 @@
-class AddLoginIdToReviews < ActiveRecord::Migration
-  def self.up
-    add_column :reviews, :login_id, :integer
-  end
-
-  def self.down
-    remove_column :reviews, :login_id
-  end
-end
