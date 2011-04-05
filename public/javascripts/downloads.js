e4772147e1d3e3744ce1d64a1fe07287890640e8
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-04-05 15:33:13 -0700

    p 11944929 Facebook gift this book

--- public/javascripts/downloads.js
+++ public/javascripts/downloads.js
@@ -1,3 +1,4 @@
+
 $(function(){
   $('.download-link').click(function(){
     return true;
@@ -12,4 +13,43 @@ $(function(){
       url: '/bingo_experiments/create'
     });
   });
+  
+  $('#download-facebook-post.conversion-button.kiss.green.large.kiss').click(function(){
+    FB.getLoginStatus(function(response) {
+      if (response.session) {
+        downloadPagePublishToFacebook();
+      } else {
+        FB.login(function(response) {
+          if (response.session) {
+            login(response);
+            // show dialog with feed
+            downloadPagePublishToFacebook();
+          }
+        });
+      }
+    });
+   return false;
+  });
 });
+
+function downloadPagePublishToFacebook(){
+  FB.ui(
+   {
+     appId: $("meta[property='fb:app_id']").attr('content'),
+     method: 'feed',
+     link: $("meta[property='og:url']").attr('content'),
+     picture: $("meta[property='og:image']").attr('content'),
+     caption: $("meta[property='og:title']").attr('content'),
+     description: $("meta[property='og:description']").attr('content'),
+     message: "I am reading " + $("meta[property='og:title']").attr('content') + " at Classicly.com. Here is a free copy for yourself."
+   },
+   function(response) {
+     if (response && response.post_id && RAILS_ENV == 'production') {
+       _paq.push(["trackConversion", {
+          id: "8V8KUH0UQ4LD",
+          value: null
+       }]);
+     }
+   }
+ );
+}
