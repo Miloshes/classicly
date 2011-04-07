e2b80acc032c999a4c2f52400a6ddd7b4394b5be
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-04-07 13:12:44 -0700

    added code for book seo.

--- app/controllers/seo_controller.rb
+++ app/controllers/seo_controller.rb
@@ -1,9 +1,10 @@
 class SeoController < ApplicationController
-  layout :seo_layout
   # before filters to show collections lists in left nav
   before_filter :find_collection
+  before_filter :find_book_or_audio_book, :only => :book
   before_filter :find_author_collections
   before_filter :find_genre_collections
+  layout :seo_layout
 
   def show
     if @collection && @collection.book_type == 'book'
@@ -22,15 +23,31 @@ class SeoController < ApplicationController
       render :template => 'search/show'
     end
   end
-  
+
+  def book
+    if @book
+      @related_books = @book.find_fake_related(8)
+      @books_from_the_same_collection = @book.find_more_from_same_collection(2)
+      mix_panel_properties = {:title => @book.pretty_title}
+      if user_signed_in?
+        mix_panel_properties.merge!({:id => current_login.fb_connect_id})
+      end
+      @mixpanel.track_event("Viewed Book", mix_panel_properties) if Rails.env.production?
+      # if there was a failed review, it will come in the session object
+      @review = session[:review] || Review.new
+      session[:review] = nil
+      render 'books/show'
+    elsif @audio_book
+      @related_audio_books = @audio_book.find_fake_related(8)
+      @audio_books_from_the_same_collection = @audio_book.find_more_from_same_collection(2)
+      render 'audiobooks/show'
+    end
+  end
+
   private
 
   def seo_layout
-    if @collection = Collection.find(params[:id]) rescue nil
-      'collections'
-    else
-      'application'
-    end
+    @collection ? 'collections': 'application'
   end
 
   def find_collection
@@ -39,7 +56,7 @@ class SeoController < ApplicationController
   end
 
   def find_author_collections
-    if @collection && @collection.book_type == 'audiobook'
+    if (@collection && @collection.book_type == 'audiobook') || @audio_book
       @author_collections = Collection.audio_book_type.by_author
     else
       @author_collections = Collection.book_type.by_author
@@ -47,11 +64,18 @@ class SeoController < ApplicationController
   end
 
   def find_genre_collections
-    if @collection && @collection.book_type == 'audiobook'
+    if (@collection && @collection.book_type == 'audiobook') || @audio_book
       @genre_collections = Collection.audio_book_type.by_collection
     else
       @genre_collections = Collection.book_type.by_collection
     end
   end
 
+
+  def find_book_or_audio_book
+    @book = Book.joins(:author).where(:cached_slug => params[:id], :author => {:cached_slug => params[:author_id]}).first rescue nil
+    unless @book
+      @audio_book = Audiobook.joins(:author).where(:cached_slug => params[:id], :author => {:cached_slug => params[:author_id]}).first rescue nil
+    end
+  end
 end
--- app/models/audiobook.rb
+++ app/models/audiobook.rb
@@ -2,15 +2,15 @@ class Audiobook < ActiveRecord::Base
   belongs_to :author
   belongs_to :custom_cover
 
-  has_many :audio_collection_audio_book_assignments, :foreign_key => 'audio_book_id'
-  has_many :audio_collections, :through => :audio_collection_audiobook_assignments
+  has_many :collection_audiobook_assignments
+  has_many :collections, :through => :collection_audiobook_assignments
 
   validates :title, :presence => true
 
   scope :blessed, where({:blessed => true})
   scope :random, lambda { |limit| {:order => 'RANDOM()', :limit => limit }}
 
-  has_friendly_id :pretty_title, :use_slug => true
+  has_friendly_id :audio_book_slugs, :use_slug => true
 
   def find_fake_related(num = 8)
     result = []
@@ -38,7 +38,7 @@ class Audiobook < ActiveRecord::Base
     audio_books_to_choose_from = []
 
     self.collections.each do |collection|
-      audio_books_to_choose_from += collection.audiobooks.where("audiobooks.id <> ?", self.id)
+      audio_books_to_choose_from += collection.audiobooks.where('audiobooks.id <> ?', self.id)
     end
 
     result = choose_audio_books(num, result, audio_books_to_choose_from)
@@ -60,4 +60,13 @@ class Audiobook < ActiveRecord::Base
     end
     result
   end
+
+  def audio_book_slugs
+    if Book.exists?(:pretty_title => self.pretty_title)
+      "#{pretty_title}-audiobook"
+    else
+      pretty_title
+    end
+  end
+
 end
