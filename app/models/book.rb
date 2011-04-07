86888084f4fa66ee23888b8535b4aff76e6337d6
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-04-07 09:49:46 -0700

    p12042037 outer join for searching books. This will pulll books with no associated collections

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ActiveRecord::Base
   include Descriptable
   belongs_to :author
   belongs_to :custom_cover
-  
+
   has_many :collection_book_assignments
   has_many :collections, :through => :collection_book_assignments
   has_and_belongs_to_many :genres
@@ -25,9 +25,11 @@ class Book < ActiveRecord::Base
       book.update_attribute(:available, available)
     end
   end
-  
+
   def self.search(search_term, page_num)
-    self.joins(:collections).joins(:author).where({:title.matches => "%#{search_term}%"} | 
+    self.joins('LEFT OUTER JOIN collection_book_assignments ON books.id = collection_book_assignments.book_id').
+        joins('LEFT OUTER JOIN collections ON collections.id = collection_book_assignments.collection_id').
+        joins(:author).where({:title.matches => "%#{search_term}%"} |
     {:collections => {:name.matches => "%#{search_term}%"}} |
     {:author => {:name.matches => "%#{search_term}%"}}).select('DISTINCT books.*').page(page_num).per(10)
   end
@@ -46,7 +48,7 @@ class Book < ActiveRecord::Base
     # protected URL, expires in 5 mins
     S3Object.url_for(object_key, APP_CONFIG['buckets']['books'], :expires_in => 300)
   end
-  
+
   # Reads the binary data from S3 for the book file. Needs the file format as a parameter
   def file_data_for_format(format)
     AWS::S3::Base.establish_connection!(
@@ -67,9 +69,9 @@ class Book < ActiveRecord::Base
   end
 
   def description_for_open_graph
-    "Download %s for free on Classicly - available as Kindle, PDF, Sony Reader, iBooks and more, or simply read online to your heart’s content." % self.pretty_title 
+    "Download %s for free on Classicly - available as Kindle, PDF, Sony Reader, iBooks and more, or simply read online to your heart’s content." % self.pretty_title
   end
-  
+
   def web_title
     if (self.pretty_title.length  + self.author.name.length) <= 100
       title, author = self.pretty_title, self.author.name
@@ -86,11 +88,11 @@ class Book < ActiveRecord::Base
 
   def find_fake_related(num = 8)
     result = []
-    
+
     # Two sources for related books:
     #  - popular books from the same genres with the same language
     #  - other books of the author with the same language
-    
+
     # == Popular books from the same genres (and same language)
     books_from_same_genre = []
     self.genres.each do |genre|
@@ -98,58 +100,58 @@ class Book < ActiveRecord::Base
       popular_books = genre.books.where("language = ? AND id <> ?", self.language, self.id).limit(200)
       books_from_same_genre += popular_books
     end
-    
+
     # == Other books of the author, with the same language
     books_from_same_author = self.author.books.where("language = ? AND id <> ?",  self.language, self.id)
-    
+
     # for 8 books requested we get 2 from the same author, for 2 we get 1
     num_of_books_to_get_from_same_author = (num / 4.0).ceil
-    
+
     1.upto num_of_books_to_get_from_same_author do
       break if books_from_same_author.blank?
-      
+
       position = rand(books_from_same_author.size)
       result << books_from_same_author[position]
       books_from_same_author.delete_at(position)
     end
-    
+
     1.upto num-result.size do
       position = rand(books_from_same_genre.size)
       result << books_from_same_genre[position]
       books_from_same_genre.delete_at(position)
     end
-    
+
     result.compact!
     result.sort_by { rand }
-    
+
     return result
   end
-  
+
   # NOTE: in case the result set is empty, it should fall back to books from the same author, or just blessed books
   def find_more_from_same_collection(num = 2)
     result = []
-    
+
     # == books from the same collection
     books_to_choose_from = []
     self.collections.each do |collection|
       books_to_choose_from += collection.books.where("books.id <> ?", self.id)
     end
-    
+
     1.upto num do
       break if books_to_choose_from.blank?
-      
+
       position = rand(books_to_choose_from.size)
       result << books_to_choose_from[position]
       books_to_choose_from.delete_at(position)
     end
-    
+
     # == books from the same author as a fallback
-    
+
     books_to_choose_from = self.author.books.where("id <> ?", self.id)
-    
+
     1.upto num-result.size do
       break if books_to_choose_from.blank?
-      
+
       position = rand(books_to_choose_from.size)
       result << books_to_choose_from[position]
       books_to_choose_from.delete_at(position)
@@ -157,15 +159,15 @@ class Book < ActiveRecord::Base
 
     # == blessed books
     books_to_choose_from = Book.where("id <> ? AND blessed = ?", self.id, true)
-    
+
     1.upto num-result.size do
       break if books_to_choose_from.blank?
-      
+
       position = rand(books_to_choose_from.size)
       result << books_to_choose_from[position]
       books_to_choose_from.delete_at(position)
     end
-    
+
     return result
   end
 
@@ -173,7 +175,7 @@ class Book < ActiveRecord::Base
     self.avg_rating = self.reviews.blank? ? 0 : (self.reviews.sum('rating').to_f / self.reviews.size.to_f).round
     self.save
   end
-  
+
   def has_rating?
     self.avg_rating > 0
   end
@@ -190,11 +192,11 @@ class Book < ActiveRecord::Base
     end_of_string = self.cached_slug =~ /--[\d]+/
     self.cached_slug[0, end_of_string]
   end
-  
+
   def self.update_description_from_web_api(data)
     book  = Book.find(data['book_id'].to_i)
     return if book.blank?
-    
+
     book.update_attributes(:description => data['description'])
   end
 end
