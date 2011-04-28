018b28a5dadf4374724d809c3aaedfbf5608d42e
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-04-28 10:08:59 -0700

    p10541733 Added left nav to resemble main design

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,5 +1,9 @@
 class BlogController < ApplicationController
   layout 'blog'
+
+  before_filter :find_author_collections
+  before_filter :find_genre_collections
+
   def index
     @posts = BlogPost.all
   end
