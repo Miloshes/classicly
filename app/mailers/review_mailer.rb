425fa2989a838590f0d21045fd1ef6f3353f63ea
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-05-05 09:50:32 -0700

    modified mailer to avoid deprecation warnings about body variables

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -1,9 +1,10 @@
 class ReviewMailer < ActionMailer::Base
+
   def notification_on_flowdock(review)
-    recipients "spreadsong <main@spreadsong.flowdock.com>"
-    from       "Classicly.com"
-    subject    "A new review has been posted"
-    sent_on    Time.now
-    body       :review => review
+    @review = review
+    mail  :to => "spreadsong <main@spreadsong.flowdock.com>",
+          :from => "Classicly.com",
+          :subject => "A new review has been posted",
+          :sent_on => Time.now
   end
 end
