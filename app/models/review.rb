649d1dad69a86a081881ca9fda693bcbe48ca88b
Ignacio De La Madrid <ignacio.delamadrid@spreadsong.com>
2011-05-05 10:29:41 -0700

    validate presence of content for review. server side

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,9 +2,10 @@ class Review < ActiveRecord::Base
   belongs_to :reviewable, :polymorphic => true
   belongs_to :reviewer, :class_name => 'Login', :foreign_key => 'login_id'
 
+  validates_presence_of :content
   validates :rating, :presence => true, :numericality => true
 
-  after_create :deliver_review_created_notification_to_flowdock
+  #after_create :deliver_review_created_notification_to_flowdock
 
   def self.create_or_update_from_ios_client_data(data)
     if data['book_id']
