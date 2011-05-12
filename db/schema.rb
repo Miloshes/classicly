55d55c969588ffbff7442baef8a8135e3d3b86bd
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-05-12 18:16:53 +0200

    updating the schema

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,17 @@ ActiveRecord::Schema.define(:version => 20110502224641) do
     t.datetime "updated_at"
   end
 
+  create_table "book_pages", :force => true do |t|
+    t.integer "book_id"
+    t.integer "page_number"
+    t.integer "first_character"
+    t.integer "last_character"
+    t.text    "content"
+    t.boolean "first_line_indent", :default => false, :null => false
+    t.boolean "re_render_flag",    :default => false, :null => false
+    t.boolean "force_rerender",    :default => false, :null => false
+  end
+
   create_table "books", :force => true do |t|
     t.text    "title"
     t.integer "author_id"
@@ -74,8 +85,8 @@ ActiveRecord::Schema.define(:version => 20110502224641) do
     t.string  "pretty_title"
     t.boolean "available",        :default => true
     t.string  "cached_slug"
-    t.integer "avg_rating"
     t.integer "downloaded_count", :default => 0
+    t.integer "avg_rating",       :default => 0,     :null => false
   end
 
   create_table "books_genres", :id => false, :force => true do |t|
@@ -96,36 +107,36 @@ ActiveRecord::Schema.define(:version => 20110502224641) do
   end
 
   create_table "collections", :force => true do |t|
-    t.string   "name"
-    t.string   "book_type"
-    t.string   "collection_type"
-    t.string   "paperback_color"
-    t.string   "source_type"
-    t.text     "description"
-    t.text     "source"
-    t.boolean  "has_image",                    :default => false, :null => false
-    t.boolean  "featured",                     :default => false, :null => false
-    t.datetime "created_at"
-    t.string   "author_portrait_file_name"
-    t.string   "author_portrait_content_type"
-    t.integer  "author_portrait_file_size"
-    t.datetime "author_portrait_updated_at"
-    t.integer  "genre_id"
-    t.string   "cached_slug"
-    t.integer  "downloaded_count",             :default => 0
+    t.string    "name"
+    t.string    "book_type"
+    t.string    "collection_type"
+    t.string    "paperback_color"
+    t.string    "source_type"
+    t.text      "description"
+    t.text      "source"
+    t.boolean   "has_image",                    :default => false, :null => false
+    t.boolean   "featured",                     :default => false, :null => false
+    t.timestamp "created_at"
+    t.string    "author_portrait_file_name"
+    t.string    "author_portrait_content_type"
+    t.integer   "author_portrait_file_size"
+    t.timestamp "author_portrait_updated_at"
+    t.integer   "genre_id"
+    t.string    "cached_slug"
+    t.integer   "downloaded_count",             :default => 0
   end
 
   create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",   :default => 0
-    t.integer  "attempts",   :default => 0
-    t.text     "handler"
-    t.text     "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_at"
-    t.datetime "failed_at"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "priority",   :default => 0
+    t.integer   "attempts",   :default => 0
+    t.text      "handler"
+    t.text      "last_error"
+    t.timestamp "run_at"
+    t.timestamp "locked_at"
+    t.timestamp "failed_at"
+    t.string    "locked_by"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
@@ -175,6 +186,8 @@ ActiveRecord::Schema.define(:version => 20110502224641) do
     t.text     "content"
     t.integer  "rating"
     t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "user_id"
     t.integer  "login_id"
   end
 
@@ -188,24 +201,32 @@ ActiveRecord::Schema.define(:version => 20110502224641) do
   add_index "seo_slugs", ["slug"], :name => "index_seo_slugs_on_slug"
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "session_id", :null => false
+    t.text      "data"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
-  create_table "slugs", :force => true do |t|
-    t.string   "name"
-    t.integer  "sluggable_id"
-    t.integer  "sequence",                     :default => 1, :null => false
-    t.string   "sluggable_type", :limit => 40
-    t.string   "scope"
+  create_table "shortened_urls", :force => true do |t|
+    t.text     "url"
     t.datetime "created_at"
+    t.datetime "last_hit"
+    t.integer  "hit_count",  :default => 0
   end
 
+  create_table "slugs", :force => true do |t|
+    t.string    "name"
+    t.integer   "sluggable_id"
+    t.integer   "sequence",                     :default => 1, :null => false
+    t.string    "sluggable_type", :limit => 40
+    t.string    "scope"
+    t.timestamp "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
   add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
   create_table "users", :force => true do |t|
