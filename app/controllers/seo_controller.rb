8b6b225694623d97d71902ba259073747bfe8db3
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-23 17:53:16 -0500

    redirect book page to read online page

--- app/controllers/seo_controller.rb
+++ app/controllers/seo_controller.rb
@@ -1,16 +1,12 @@
 class SeoController < ApplicationController
-  before_filter :find_book_or_audio_book, :only => :show_book
   layout 'new_design'
+
   def show_book
-    if @book
-      @related_books = @book.find_fake_related(8)
-      @books_from_the_same_collection = @book.find_more_from_same_collection(2)
-      @book.log_book_view_in_mix_panel(current_login.try(:fb_connect_id), @mixpanel)
-      set_collections_and_audibly_for_book(@book)
-      @review = session[:review] || Review.new
-      session[:review] = nil
-      render @book.class == Book ? 'books/show' : 'audiobooks/show'
-    end
+    @book = Book.joins(:author).where(:cached_slug => params[:id], :author => {:cached_slug => params[:author_id]}).first  ||
+        Audiobook.joins(:author).where(:cached_slug => params[:id], :author => {:cached_slug => params[:author_id]}).first
+    
+    slug = @book.seo_slugs.where(:format => 'online').first.slug
+    redirect_to "/#{slug}"
   end
 
   def show
@@ -36,12 +32,6 @@ class SeoController < ApplicationController
   def find_genre_collections(type)
     return  Collection.audio_book_type.by_collection if type == :audiobook
     Collection.book_type.by_collection
-  end
-
-
-  def find_book_or_audio_book
-    @book = Book.joins(:author).where(:cached_slug => params[:id], :author => {:cached_slug => params[:author_id]}).first  ||
-        Audiobook.joins(:author).where(:cached_slug => params[:id], :author => {:cached_slug => params[:author_id]}).first
   end
 
   def render_search
