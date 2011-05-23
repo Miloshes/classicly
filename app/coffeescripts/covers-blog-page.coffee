578d3241961ee9cd4441d7561ef3a6d4191f10e7
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-23 12:37:52 -0500

    load images for related books in blog page

---
+++ app/coffeescripts/covers-blog-page.coffee
@@ -0,0 +1,17 @@
+$ ->
+  # fetch found books ids
+  domElements = $('.related .book-cover')
+  # get all collection's ids:
+  allIds = domElements.map ->
+    $(this).attr('id').split('_')[1]
+  allIds = allIds.get()
+  console.log allIds
+  # create a single string:
+  data = $(allIds).get().join(',')
+  
+  $.getJSON '/json_books/', {id: data}, (data) ->
+    $.each data, ( index, value ) ->
+      id = value.attrs.id
+      selector = '.related .book-cover#book_' + id + ' .cover-here'
+      toTake = [value.attrs]
+      setElementCover( $( selector ), toTake )
---
+++ public/javascripts/covers-blog-page.js
@@ -0,0 +1,26 @@
+(function(){
+  $(function() {
+    var allIds, data, domElements;
+    // fetch found books ids
+    domElements = $('.related .book-cover');
+    // get all collection's ids:
+    allIds = domElements.map(function() {
+      return $(this).attr('id').split('_')[1];
+    });
+    allIds = allIds.get();
+    console.log(allIds);
+    // create a single string:
+    data = $(allIds).get().join(',');
+    return $.getJSON('/json_books/', {
+      id: data
+    }, function(data) {
+      return $.each(data, function(index, value) {
+        var id, selector, toTake;
+        id = value.attrs.id;
+        selector = '.related .book-cover#book_' + id + ' .cover-here';
+        toTake = [value.attrs];
+        return setElementCover($(selector), toTake);
+      });
+    });
+  });
+})();
