fa01a6f67819f918ee3b809664425439f8389dff
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-27 17:42:38 -0500

    commented out blogposts migration to allow other migrations to run

--- db/migrate/20110419172445_create_blog_posts.rb
+++ db/migrate/20110419172445_create_blog_posts.rb
@@ -1,15 +1,15 @@
-class CreateBlogPosts < ActiveRecord::Migration
-  def self.up
-    create_table :blog_posts do |t|
-      t.string :title
-      t.text :content
-      t.string :keywords
-
-      t.timestamps
-    end
-  end
-
-  def self.down
-    drop_table :blog_posts
-  end
-end
+# class CreateBlogPosts < ActiveRecord::Migration
+#   def self.up
+#     create_table :blog_posts do |t|
+#       t.string :title
+#       t.text :content
+#       t.string :keywords
+# 
+#       t.timestamps
+#     end
+#   end
+# 
+#   def self.down
+#     drop_table :blog_posts
+#   end
+# end
