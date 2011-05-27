141a54a8d381f498e0a31256863c6675df6c40cc
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-27 18:09:06 -0500

    fix for sintax error in book pages table migration

--- db/migrate/20110330185002_add_first_line_indent_to_book_pages.rb
+++ db/migrate/20110330185002_add_first_line_indent_to_book_pages.rb
@@ -1,9 +1,7 @@
 class AddFirstLineIndentToBookPages < ActiveRecord::Migration
   def self.up
-    change_table :book_pages do |t|
-      unless column_exists? :first_line_indent
-        t.boolean :first_line_indent, :default => false, :null => false
-      end
+    unless column_exists? :book_pages, :first_line_indent
+      add_column :book_pages, :first_line_indent, :boolean, :default => false, :null => false
     end
   end
 
