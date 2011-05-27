31bae16c371398b5a2317e24ad0f9d8d495a7abf
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-27 18:06:58 -0500

    fix for sintax error in previous commit

--- db/migrate/20110428121330_add_avg_rating_to_audiobooks.rb
+++ db/migrate/20110428121330_add_avg_rating_to_audiobooks.rb
@@ -1,9 +1,7 @@
 class AddAvgRatingToAudiobooks < ActiveRecord::Migration
   def self.up
-    change_table :audiobooks do |t|
-      unless column_exists? :avg_rating
-        t.integer :avg_rating, :default => 0, :null => false
-      end
+    unless column_exists? :audiobooks, :avg_rating
+      add_column :audiobooks, :avg_rating, :integer, :default => 0, :null => false
     end
   end
 
