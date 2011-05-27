715f3a7c97b560d92744eb22b5aa73d4ec3e9034
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-27 17:55:17 -0500

    Avoid sql collisions for existing tables

--- db/migrate/20110330092901_create_book_pages.rb
+++ db/migrate/20110330092901_create_book_pages.rb
@@ -1,15 +1,17 @@
-# class CreateBookPages < ActiveRecord::Migration
-#   def self.up
-#     create_table :book_pages do |t|
-#       t.integer :book_id
-#       t.integer :page_number
-#       t.integer :first_character
-#       t.integer :last_character
-#       t.text :content
-#     end
-#   end
-# 
-#   def self.down
-#     drop_table :book_pages
-#   end
-# end
+class CreateBookPages < ActiveRecord::Migration
+  def self.up
+    unless table_exists? :book_pages
+      create_table :book_pages do |t|
+        t.integer :book_id
+        t.integer :page_number
+        t.integer :first_character
+        t.integer :last_character
+        t.text :content
+      end
+    end
+  end
+
+  def self.down
+    drop_table :book_pages
+  end
+end
--- db/migrate/20110419172445_create_blog_posts.rb
+++ db/migrate/20110419172445_create_blog_posts.rb
@@ -1,15 +1,17 @@
-# class CreateBlogPosts < ActiveRecord::Migration
-#   def self.up
-#     create_table :blog_posts do |t|
-#       t.string :title
-#       t.text :content
-#       t.string :keywords
-# 
-#       t.timestamps
-#     end
-#   end
-# 
-#   def self.down
-#     drop_table :blog_posts
-#   end
-# end
+class CreateBlogPosts < ActiveRecord::Migration
+  def self.up
+    unless table_exists? :blog_posts 
+      create_table :blog_posts do |t|
+        t.string :title
+        t.text :content
+        t.string :keywords
+
+        t.timestamps
+      end
+    end
+  end
+
+  def self.down
+    drop_table :blog_posts
+  end
+end
