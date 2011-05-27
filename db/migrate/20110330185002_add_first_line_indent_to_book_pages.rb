1342c8ffcfd68bbb6e61f286bcead677f9ee3e57
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-05-27 17:59:40 -0500

    Avoid sql collisions for existing column in book pages table

--- db/migrate/20110330185002_add_first_line_indent_to_book_pages.rb
+++ db/migrate/20110330185002_add_first_line_indent_to_book_pages.rb
@@ -1,6 +1,10 @@
 class AddFirstLineIndentToBookPages < ActiveRecord::Migration
   def self.up
-    add_column :book_pages, :first_line_indent, :boolean, :default => false, :null => false
+    change_table :book_pages do |t|
+      unless column_exists? :first_line_indent
+        t.boolean :first_line_indent, :default => false, :null => false
+      end
+    end
   end
 
   def self.down
