495b7b419c42fdc6dda844f8cf42b32cc900b4a2
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-06-05 00:38:54 +0200

    HTTP caching collection json response

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,10 +1,14 @@
 class CollectionsController < ApplicationController
   
-  # receives an array of collection ids separated by comas
+  # receives an array of collection ids separated by commas
   # return a json array with collection id and its books
-  def collection_json_books 
+  def collection_json_books
+    # cache response for a week
+    response.headers['Cache-Control'] = "public, max-age=#{7*24*60*60}"
+    
     collection_ids = params[:id].split( ',' )
     data = Collection.get_collection_books_in_json collection_ids, params[:type]
     render :json => data.to_json
   end
+  
 end
