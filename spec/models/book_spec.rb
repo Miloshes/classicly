89d5cc1081295d77b0aac6c56e1cc8a754cf983b
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-06-23 17:55:04 -0500

    add download as mp3 for those books that have an audiobook

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -15,4 +15,43 @@ describe Book do
       @book.read_online?.should be_false
     end
   end
+  
+  describe '#audiobook_download_slug' do
+    before :each do
+      @book = Book.make!(:pretty_title => 'Les miserables')
+    end
+    
+    context 'when there is no audiobook for the current book' do
+      it 'should return nil' do
+        @book.audiobook_download_slug.should be_nil
+      end
+      
+      it 'should return nil even if the db is corrupted' do
+        the_book = Book.find @book.id
+        the_book.update_attribute :has_audiobook, true
+        the_book.audiobook_download_slug.should be_nil
+      end
+    end
+    
+    describe 'when there is an audiobook correspondant to the book' do
+      before :each do
+        @audiobook = Audiobook.make!(:pretty_title => 'Les miserables', :cached_slug => 'les-miserables-audiobook')
+        SeoSlug.create!(:seoable => @audiobook, :format => 'mp3', :slug => 'download-les-miserables-audiobook-mp3')
+      end
+      context 'when the database has been corrupted and the book does not report that the audiobook exists' do
+        it 'should return nil' do
+          the_book = Book.find @book.id
+          the_book.update_attribute :has_audiobook, false
+          @book.audiobook_download_slug.should be_nil
+        end
+      end
+      context 'when the db and the slug are present' do
+        it 'should return the download mp3 slug' do
+          the_book = Book.find @book.id
+          the_book.update_attribute :has_audiobook, true
+          the_book.audiobook_download_slug.should == 'download-les-miserables-audiobook-mp3'
+        end
+      end
+    end
+  end
 end
