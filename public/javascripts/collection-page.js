edd90b0950152f8ee4888468fcbb3ed0bdb49146
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-07-08 12:10:36 -0500

    deleted unused javascripts

--- public/javascripts/collection-page.js
+++
@@ -1,8 +0,0 @@
-// $(function(){
-// 
-//   // if the user has signed in and the form is not shown yet , show it
-//   FB.Event.subscribe('auth.login', function(response) {
-//     login(response);
-//   });
-// 
-// });
