5af168d2a60f883ebc479fea1f82015a515f4418
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-07-13 18:28:42 +0200

    adding book count, removing faulty line

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,16 +1,19 @@
 class LibrariesController < ApplicationController
 
   def show
-    current_library.books = Book.where(:title.matches => "Dracula").all()
-    # we get here via a redirect. We're listing the library, but the previous action might've just put a book in here which we get to know via a session
+    # We're listing the library, but the previous action might've just put a book in here which we get to know via a session
     if session[:new_book_in_library]
       @new_book_in_library = Book.find(session[:new_book_in_library])
+
       current_library.books << @new_book_in_library
+      current_library.books_downloaded = current_library.books.count
+
       @download_format = session[:download_format_for_the_new_book]
+
       session[:new_book_in_library] = nil
       session[:download_format_for_the_new_book] = nil
-      session[:library] = nil # otherwise, it will create a dumpfile error
     end
+    
     @books = current_library.books
   end
   
