f18c476345d503e8c774e032e784b834bbdd88a5
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-07-20 15:27:36 +0200

    model update for the Library

--- app/models/library.rb
+++ app/models/library.rb
@@ -1,10 +1,10 @@
 class Library < ActiveRecord::Base  
   belongs_to :user, :class_name => 'Login', :foreign_key => 'login_id'
   
-  has_many :library_books
+  has_many :library_books, :dependent => :destroy
   has_many :books, :through => :library_books
   
-  has_many :library_audiobooks
+  has_many :library_audiobooks, :dependent => :destroy
   has_many :audiobooks, :through => :library_audiobooks
   
   has_one :last_read_book, :through => :library_books, :order => 'library_books.last_opened DESC', :source => :book
