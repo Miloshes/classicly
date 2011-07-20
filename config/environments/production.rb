cd808cf28a50877523b8a0920c397a6b358a0754
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-07-20 12:25:52 -0500

    new version of the redirect to www version on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,7 @@ Classicly::Application.configure do
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
 
+  config.middleware.use 'WwwMiddleware'
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
   config.serve_static_assets = false
