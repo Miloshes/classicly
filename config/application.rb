e1ee9b7e3f112585a1828acc98a6a50fc6151082
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-07-20 12:25:52 -0500

    new version of the redirect to www version on production

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Classicly
     # -- all .rb files in that directory are automatically loaded.
 
     # Custom directories with classes and modules you want to be autoloadable.
-    config.autoload_paths += %W(#{config.root}/lib/common)
+    config.autoload_paths += %W(#{config.root}/lib/common #{config.root}/lib/middleware)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
