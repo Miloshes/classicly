6e44e2f3e2c3eecfa9191efc72c1b03f5a69ea6d
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-07-22 14:58:32 +0200

    changing the colors and animation for the now downloading notification

--- public/javascripts/notifications.js
+++ public/javascripts/notifications.js
@@ -1,3 +1,4 @@
+// call it with animate=true if you want it to animate
 function hideGlobalNotifications(animate) {
 	$('.global-notification').each( function() {		
 		var animation_length = 0;
@@ -7,17 +8,55 @@ function hideGlobalNotifications(animate) {
 		}
 		
 		if ( $(this).is(':visible') ) {
-				$(this).animate({'top': '-=' + $(this).css('height')}, animation_length, function() { $(this).hide(); });
+			$(this).animate(
+				{
+					'top': '-=' + $(this).outerHeight()
+				},
+				{
+					duration: animation_length,
+			    easing: 'swing',
+					complete: function() { $(this).hide(); }
+				}
+			);
+			$('#site-container').animate(
+				{
+					'top': '-=' + $(this).outerHeight()
+				},
+				{
+					duration: animation_length,
+					easing: 'swing'
+				}
+			);
 		}
 	});
 }
 
 function showGlobalNotifications() {
 	$('.global-notification').each( function() {
+		
+		animation_length = 500;
+		
 		// if it's not visible, animate it in
 		if ( !$(this).is(':visible') ) {
 			$(this).show();
-			$(this).animate({'top': '+=' + $(this).css('height')}, 500);
+			$(this).animate(
+				{
+					'top': '+=' + $(this).outerHeight()
+				},
+				{
+					duration: animation_length,
+			    easing: 'swing'
+				}
+			);
+			$('#site-container').animate(
+				{
+					'top': '+=' + $(this).outerHeight()
+				},
+				{
+					duration: animation_length,
+					easing: 'swing'
+				}
+			);
 		}
 	});
 }
