58c641e3c713334ad3b100b01027f2afece62c56
Ignacio De La Madrid <ignacio@spreadsong.com>
2011-08-22 14:27:31 -0500

    ratings UI.Show correct rating for user

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,5 +2,4 @@ class Rating < ActiveRecord::Base
   belongs_to :rateable, :polymorphic => true
   belongs_to :rater, :class_name => "Login", :foreign_key => 'login_id'
   validates_presence_of :score
-
 end
