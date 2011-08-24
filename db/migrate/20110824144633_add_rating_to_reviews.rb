701799b37c3863ba9f6d77674def61c8fa3b0a84
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2011-08-24 10:49:37 -0500

    delete unnecessary migrations to avoid messing up production db

--- db/migrate/20110824144633_add_rating_to_reviews.rb
+++
@@ -1,9 +0,0 @@
-class AddRatingToReviews < ActiveRecord::Migration
-  def self.up
-    add_column :reviews, :rating, :integer, :default => 0 
-  end
-
-  def self.down
-    remove_column :reviews, :rating
-  end
-end
