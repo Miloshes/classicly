500b2119ca550f57abaa08474c7cdcead24d946e
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2011-09-02 18:40:37 -0500

    interaction for ratings in collection book list views

--- public/javascripts/ratings-book-list-view.js
+++ public/javascripts/ratings-book-list-view.js
@@ -1,17 +1,24 @@
 $( function(){
-  if( !( isLoggedIn() ) )
-    $( 'input.dynamic-stars' ).rating('readOnly', true);
   
   $( "#fb_connect_notification a#fb_connect" ).click( function(){
+    
     FB.login(function( response ) {
+      // send the rating if logged in:
+      if (response.session) {
+        var bookId = $( 'ul.book-list' ).data( 'selectedBookId' );
+        var rating = $( 'ul.book-list' ).data( 'selectedBookRating' );
+        
+        sendRating( bookdId, rating );
+      }
+      // hide the rtegistration dropdown:
       if ( $( "#fb_connect_notification" ).is( ":visible" ) )
         $( "#fb_connect_notification ").slideUp( "slow" );
-      $( 'input.dynamic-stars' ).rating('readOnly', false);
     });
     return false;
   });
 
 });
+
 
 $( 'input.dynamic-stars' ).rating({callback: function(value, link ){
   var domId = $( this ).attr( 'id' );
@@ -21,14 +28,58 @@ $( 'input.dynamic-stars' ).rating({callback: function(value, link ){
   var data = 'book_id=' + bookId + '&rating=' + value;
 
   // send the rating
+  if( isLoggedIn() ){
+    sendRating( bookId, value );
+  }else{
+    // show the registration dropdown:
+    if ( $( "#fb_connect_notification" ).is( ":hidden" ) )
+      $( "#fb_connect_notification ").slideDown( "slow" );
+
+    // store temporarily request's data. This because once the user logs in, this data will be send to create the rating:
+    $( 'ul.book-list' ).data( 'selectedBookId' , bookId );
+    $( 'ul.book-list' ).data( 'selectedBookRating' , value );
+  }
+}});
+
+// this is for the ratings plugin. It avoids showing a non useful delete icon:
+$( '.rating-cancel' ).remove();
+
+function sendRating( bookId, rating ){
+  var data = 'book_id=' + bookId + '&rating=' + rating;
+  
   $.ajax({
     type: 'POST',
     url: '/reviews/create_rating',
     data: data,
+    beforeSend: hideStarsOnRating( bookId ),
     success: function(){
+      myReviewText = $( "li#book_" + bookId + " .row .my-rating-text" );
+      myReviewText.text( "Saved!" );
+      setTimeout( "showStarsOnRatingCompleted(" + bookId + ")", 200 );
     }
   });
- 
-}});
+}
 
-$( '.rating-cancel' ).remove();
+function hideStarsOnRating( bookId ){
+  myReviewText = $( "li#book_" + bookId + " .row .my-rating-text" );
+  myReviewText.text( "Saving..." );
+  
+  // traverse DOM:
+  parent = myReviewText.parents( '.download-link' );
+  stars = parent.children( '.row.stars' );
+  
+  stars.children( '.columns.rating ' ).hide();
+  stars.children( '.columns.blank-stars' ).show();
+}
+
+function showStarsOnRatingCompleted( bookId ){
+  myReviewText = $( "li#book_" + bookId + " .row .my-rating-text" );
+  myReviewText.text( "My Rating:" );
+  
+  // traverse DOM:
+  parent = myReviewText.parents( '.download-link' );
+  stars = parent.children( '.row.stars' );
+  
+  stars.children( '.columns.rating ' ).show();
+  stars.children( '.columns.blank-stars' ).hide();
+}
