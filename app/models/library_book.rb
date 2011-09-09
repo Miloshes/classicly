e03e0e10a9aa5542e0cf5517c2850e7522957059
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-09-09 16:17:48 +0200

    library join models tweak

--- app/models/library_book.rb
+++ app/models/library_book.rb
@@ -3,7 +3,7 @@ class LibraryBook < ActiveRecord::Base
   belongs_to :library
   has_many :bookmarks
 
-  def is_bookmarked_at? page_number
+  def is_bookmarked_at?(page_number)
     self.bookmarks.exists? :page_number => page_number
   end
 
