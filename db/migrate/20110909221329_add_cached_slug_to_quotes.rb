1e019422be24c77be23e57768211bbed6c36f122
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2011-09-16 09:38:03 -0500

    temporarily comment migration to allow db migrate run

--- db/migrate/20110909221329_add_cached_slug_to_quotes.rb
+++ db/migrate/20110909221329_add_cached_slug_to_quotes.rb
@@ -1,4 +1,4 @@
-# class AddCachedSlugToQuotes < ActiveRecord::Migration
+class AddCachedSlugToQuotes < ActiveRecord::Migration
 #   def self.up
 #     add_column :quotes, :cached_slug, :string
 #   end
@@ -6,4 +6,4 @@
 #   def self.down
 #     remove_column :quotes, :cached_slug
 #   end
-# end
+end
