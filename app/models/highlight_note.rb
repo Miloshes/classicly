cfc696241dad081910d761ab486ee9b84c1ef024
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-10-05 15:39:27 +0200

    removing the HighlightNote model

--- app/models/highlight_note.rb
+++
@@ -1,4 +0,0 @@
-class HighlightNote < ActiveRecord::Base
-  belongs_to :noteable, :polymorphic => true
-  belongs_to :note_writer, :class_name => "Login", :foreign_key => 'login_id'
-end
--- spec/models/highlight_note_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe HighlightNote do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
