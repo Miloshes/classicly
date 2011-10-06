4622c381c6bacedf082b0f6c703f05935e37a3a4
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2011-10-06 16:24:58 -0500

    bind the message habler zsolt built to tweet for highlight

--- app/controllers/book_highlights_controller.rb
+++ app/controllers/book_highlights_controller.rb
@@ -5,6 +5,10 @@ class BookHighlightsController < ApplicationController
     @book = Book.find(params[:book_id])
     # More probably, it's an anonymous highlight. If it isn't, we take a BookHighlight
     @highlight = AnonymousBookHighlight.find_by_cached_slug(params[:highlight_id]) || BookHighlight.find_by_cached_slug(params[:highlight_id])
+    
+    message_handler = ShareMessageHandler.new
+    @message_for_twitter = message_handler.get_message_for({:target_platform => 'twitter', 
+      :message_type => 'highlight share', :highlight => @highlight})
   end
 
 end
