5d2ca4488eaa4d302febe9fb9708f4c3c96f121a
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-10-19 11:04:47 +0200

    adding security to the API v1.3

--- app/controllers/web_api_controller.rb
+++ app/controllers/web_api_controller.rb
@@ -7,6 +7,8 @@ class WebApiController < ApplicationController
   skip_before_filter :popular_collections
   
   before_filter :get_api_handler
+  before_filter :fetch_api_params
+  before_filter :authenticate_api_call
   
   def create
     response = @handler.handle_incoming_data(params)
@@ -25,6 +27,21 @@ class WebApiController < ApplicationController
   
   def get_api_handler
     @handler = WebApiHandler.new
+  end
+  
+  def fetch_api_params
+    @parsed_data = params["json_data"].blank? ? {} : ActiveSupport::JSON.decode(params["json_data"]).stringify_keys
+  end
+  
+  def authenticate_api_call
+    # introducing authentication for API version 1.3 and above
+    return true if @parsed_data["structure_version"] != "1.3"
+    
+    correct_signature = Digest::MD5.hexdigest((params["json_data"] || "") + APP_CONFIG["api_secret"])
+    
+    unless (params["api_key"] == APP_CONFIG["api_key"] && params["api_signature"] == correct_signature)
+      render :nothing => true, :status => :unauthorized
+    end
   end
   
 end
