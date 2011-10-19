6f053c51776b2f931c6931cd4e965704ddf64747
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-10-19 11:05:28 +0200

    full test-suite for the classicly account's web API

--- spec/controllers/web_api_for_account_handling_spec.rb
+++ spec/controllers/web_api_for_account_handling_spec.rb
@@ -1,9 +1,9 @@
 describe WebApiController, "(API calls - account handling related)" do
   
   describe "keeping compatibility with the old registration system" do
-  
-    it "should be able to register an iOS user" do
-      data = {
+    
+    before(:each) do
+      @data = {
           "structure_version"     => "1.2",
           "action"                => "register_ios_user",
           "user_fbconnect_id"     => "1232134",
@@ -14,8 +14,10 @@ describe WebApiController, "(API calls - account handling related)" do
           "user_location_city"    => "Budapest",
           "user_location_country" => "Hungary"
         }
-    
-      post "create", :json_data => data.to_json
+    end
+  
+    it "should be able to register an iOS user" do
+      post "create", :json_data => @data.to_json
     
       Login.should have(1).record
     end
@@ -35,14 +37,20 @@ describe WebApiController, "(API calls - account handling related)" do
       parsed_response["email"].should == "email_to_check@test.com"
       parsed_response["first_name"].should == "Zsolt"
     end
+    
+    it "should use the Facebook ID as the main means of retrieving a user" do
+      Login.should_receive(:where).with(hash_including(:fb_connect_id)).and_return([])
+      
+      post "create", :json_data => @data.to_json
+    end
   
   end
   
-  describe "registering a classicly account" do
+  describe "a classicly account" do
     
     before(:each) do
       @data = {
-          "structure_version"     => "1.2",
+          "structure_version"     => "1.3",
           "action"                => "register_ios_user",
           "user_fbconnect_id"     => "1232134",
           "device_ss_id"          => "ASDASD",
@@ -50,15 +58,139 @@ describe WebApiController, "(API calls - account handling related)" do
           "user_first_name"       => "Zsolt",
           "user_last_name"        => "Maslanyi",
           "user_location_city"    => "Budapest",
-          "user_location_country" => "Hungary"
+          "user_location_country" => "Hungary",
+          # new params for the 1.3 api
+          "twitter_name"          => "zsolt_maslanyi",
+          "password"              => "pass123",
+          "terms_of_service"      => "accepted"
         }
       
     end
     
+    describe "when getting created" do
+      
+      it "should be able to register a new Classicly account for an iOS user" do
+        post("create",
+            :json_data     => @data.to_json,
+            :api_key       => APP_CONFIG["api_key"],
+            :api_signature => Digest::MD5.hexdigest(@data.to_json + APP_CONFIG["api_secret"])
+          )
+        
+        Login.should have(1).record
+      end
+      
+      it "should respond with the account's data for a successfull registration" do
+        post("create",
+            :json_data     => @data.to_json,
+            :api_key       => APP_CONFIG["api_key"],
+            :api_signature => Digest::MD5.hexdigest(@data.to_json + APP_CONFIG["api_secret"])
+          )
+        
+        parsed_response = ActiveSupport::JSON.decode(response.body)
+        
+        parsed_response.class.should == Hash
+        parsed_response.keys.sort.should == %w(email fb_connect_id first_name last_name location_city location_country twitter_name)
+      end
+      
+      it "should respond with failure when trying to register an existing account" do
+        # we have an account that's fully registered
+        login = mock_model(Login, :not_a_real_account => false)
+        Login.stub_chain(:where, :first).and_return(login)
+        
+        post("create",
+            :json_data     => @data.to_json,
+            :api_key       => APP_CONFIG["api_key"],
+            :api_signature => Digest::MD5.hexdigest(@data.to_json + APP_CONFIG["api_secret"])
+          )
+        
+        parsed_response = ActiveSupport::JSON.decode(response.body)
+        
+        parsed_response.should == "FAILURE"
+      end
+      
+      it "should migrate half-accounts" do
+        login = FactoryGirl.create(:login, :fb_connect_id => "123", :first_name => "Zsolt", :email => "email_to_check@test.com")
+        Login.stub_chain(:where, :first).and_return(login)
+  
+        post("create",
+            :json_data     => @data.to_json,
+            :api_key       => APP_CONFIG["api_key"],
+            :api_signature => Digest::MD5.hexdigest(@data.to_json + APP_CONFIG["api_secret"])
+          )
+        
+        login.not_a_real_account.should == false
+      end
+      
+      it "should use the user email as the main means of retrieving a user" do
+        Login.should_receive(:where).with(hash_including(:email)).and_return([])
+
+        post("create",
+          :json_data     => @data.to_json,
+          :api_key       => APP_CONFIG["api_key"],
+          :api_signature => Digest::MD5.hexdigest(@data.to_json + APP_CONFIG["api_secret"])
+        )
+      end
+      
+    end
+    
+    it "should be able to do a password check for an iOS user" do
+      login = FactoryGirl.create(:login, :email => "test@test.com", :password => "pass123")
+      Login.stub_chain(:where, :first).and_return(login)
+      
+      data = {
+        "structure_version"     => "1.3",
+        "action"                => "login_ios_user",
+        "user_email"            => "test@test.com",
+        "password"              => "pass123"
+      }
+      
+      # Trying success
+      post("create",
+          :json_data     => data.to_json,
+          :api_key       => APP_CONFIG["api_key"],
+          :api_signature => Digest::MD5.hexdigest(data.to_json + APP_CONFIG["api_secret"])
+        )
+
+      parsed_response = ActiveSupport::JSON.decode(response.body)
+      parsed_response.should == "SUCCESS"
+      
+      # Trying failure - bad password
+      data["password"] = "wrong password"
+
+      post("create",
+          :json_data     => data.to_json,
+          :api_key       => APP_CONFIG["api_key"],
+          :api_signature => Digest::MD5.hexdigest(data.to_json + APP_CONFIG["api_secret"])
+        )
+        
+      parsed_response = ActiveSupport::JSON.decode(response.body)
+      parsed_response.should == "FAILURE"
+    end
+    
+    # TODO: later
+    # it "should have a reset password functionality"
+    
+    it "should be able to give information about a user" do
+      login = FactoryGirl.create(:login, :fb_connect_id => "123", :first_name => "Zsolt", :email => "email_to_check@test.com")
+    
+      data = {
+          "structure_version" => "1.3",
+          "action"            => "get_user_data",
+          "user_fbconnect_id" => "123"
+        }
+    
+      post("query",
+          :json_data     => data.to_json,
+          :api_key       => APP_CONFIG["api_key"],
+          :api_signature => Digest::MD5.hexdigest(data.to_json + APP_CONFIG["api_secret"])
+        )
+    
+      parsed_response = ActiveSupport::JSON.decode(response.body)
+    
+      parsed_response["email"].should == "email_to_check@test.com"
+      parsed_response["first_name"].should == "Zsolt"
+    end
+    
   end
-  
-  describe "updating a classicly account"
-  
-  describe "logging in - "
 
 end
