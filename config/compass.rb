de9c92ed1c298546ec2e2d73ee01bf7f6f7f0820
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-10-26 15:35:11 +0200

    fixing the header

--- config/compass.rb
+++ config/compass.rb
@@ -1,6 +1,20 @@
 project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 http_path = "/"
-css_dir = "tmp/stylesheets"
-sass_dir = "app/views/stylesheets"
+
+if Rails.env == "development"
+  css_dir = "public/stylesheets/compiled"
+else
+  css_dir = "tmp/stylesheets"
+end
+
+sass_dir = "app/stylesheets"
 environment = Compass::AppIntegration::Rails.env
+
+# Old version
+# project_type = :rails
+# project_path = Compass::AppIntegration::Rails.root
+# http_path = "/"
+# css_dir = "public/stylesheets/compiled"
+# sass_dir = "app/stylesheets"
+# environment = Compass::AppIntegration::Rails.env
