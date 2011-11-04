c52b8b8ffd8ca0d84fd7f4c2019a4e9037eef727
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-11-04 12:10:33 +0100

    adding new fields to the book delivery package to be able to identify the destiniation user better

--- db/migrate/20111005144210_create_book_delivery_packages.rb
+++ db/migrate/20111005144210_create_book_delivery_packages.rb
@@ -1,8 +1,12 @@
 class CreateBookDeliveryPackages < ActiveRecord::Migration
   def self.up
     create_table :book_delivery_packages do |t|
-      t.string :source_user
-      t.string :destination_user
+      t.integer :source_user
+      
+      t.integer :destination_user
+      t.string :destination_user_email
+      t.string :destination_user_fb_connect_id
+      
       t.string :deliverable_type
       t.integer :deliverable_id
       t.text :message
