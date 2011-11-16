0a0e16602d8e68505eb3350e5a45606afa957fa1
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-11-16 15:46:35 +0100

    email change in the registration emailer

--- app/mailers/login_mailer.rb
+++ app/mailers/login_mailer.rb
@@ -2,7 +2,8 @@ class LoginMailer < ActionMailer::Base
   def registration_notification(user)
     @user = user
     mail  :to => @user.email,
-          :from => "hello@spreadsong.com",
+          :from => "hello@classicly.com",
+          :reply_to => "colin@spreadsong.com",
           :subject => "Welcome to Classicly -- and Free Books/Free Audiobooks!",
           :date => Time.now
   end
