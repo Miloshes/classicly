245af97281692e5de7c4f905d19f11df5d04a06c
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2011-11-25 11:28:24 +0100

    hack revert

--- db/migrate/20111012170612_add_new_columns_to_login.rb
+++ db/migrate/20111012170612_add_new_columns_to_login.rb
@@ -7,7 +7,7 @@ class AddNewColumnsToLogin < ActiveRecord::Migration
   end
 
   def self.down
-    # remove_column :logins, :twitter_name
+    remove_column :logins, :twitter_name
     remove_column :logins, :salt
     remove_column :logins, :hashed_password
     remove_column :logins, :terms_of_service
