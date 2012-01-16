87c663d84efe5bb93b16ae4d762f3aaec72c2321
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2012-01-16 14:01:33 +0100

    setting default_url_options for LoginMailer

--- app/mailers/login_mailer.rb
+++ app/mailers/login_mailer.rb
@@ -1,11 +1,10 @@
 class LoginMailer < ActionMailer::Base
 
   def registration_notification_for_web(user)
+    set_url_options
+
     @user = user
-    
-    default_url_options[:host] = "www.classicly.com"
-    default_url_options[:host] = "classicly-staging.heroku.com" if Rails.env.staging?
-    
+
     mail(
         :to       => @user.email,
         :from     => "hello@classicly.com",
@@ -16,10 +15,9 @@ class LoginMailer < ActionMailer::Base
   end
   
   def registration_notification_for_ios(user)
+    set_url_options
+
     @user = user
-    
-    default_url_options[:host] = "www.classicly.com"
-    default_url_options[:host] = "classicly-staging.heroku.com" if Rails.env.staging?
     
     mail(
         :to       => @user.email,
@@ -29,4 +27,35 @@ class LoginMailer < ActionMailer::Base
         :date     => Time.now
       )
   end
+
+  def password_reset(user)
+    set_url_options
+    
+    @user = user
+    
+    mail(
+        :to       => @user.email,
+        :from     => "hello@classicly.com",
+        :reply_to => "colin@spreadsong.com",
+        :subject  => "Classicly -- trying to reset your password?",
+        :date     => Time.now
+      )
+    
+  end
+
+  private
+
+  def set_url_options
+    case Rails.env
+    when "production"
+      default_url_options[:host] = "www.classicly.com"
+    when "staging"
+      default_url_options[:host] = "classicly-staging.heroku.com"
+    when "development"
+      default_url_options[:host] = "localhost:3000"
+    when "test"
+      default_url_options[:host] = "localhost:3000"
+    end
+  end
+
 end
