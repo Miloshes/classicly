7ef3bacf6b555a0be301f471b6415c0313cffd19
Zsolt Maslanyi <zsolt.maslanyi@gmail.com>
2012-02-20 13:59:38 +0100

    logging for debugging

--- app/controllers/web_api_controller.rb
+++ app/controllers/web_api_controller.rb
@@ -30,7 +30,8 @@ class WebApiController < ApplicationController
   end
   
   def fetch_api_params
-    Rails.logger.info("\n -- got parameters: #{params["json_data"].inspect}\n")
+    Rails.logger.info("\n -- got params: #{params.inspect}\n")
+    # Rails.logger.info("\n -- got parameters: #{params["json_data"].inspect}\n")
 
     if params["json_data"].blank?
       @parsed_data = []
