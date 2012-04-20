77bfe0bc207f48c275cceb458a8842173d6a4258
qw3r <demeter.istvan@gmail.com>
2012-04-20 13:20:09 +0200

    fixing this kind of errors https://spreadsong.airbrake.io/errors/15062733  - the missing quotes wasn't handled  - now we show some "Sorry 404..." and the rest of the page remained the same, showing books of the author

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,9 +1,9 @@
 class QuotesController < ApplicationController
 
   def show
-    @collection = Collection.find(params[:id])
+    @collection = Collection.includes(:quotes).find(params[:id])
     @quote      = @collection.quotes.find_by_cached_slug(params[:quote_slug])
-    @books      = @quote.collection.books.limit(3)
+    status, @books = @quote.nil? ? [404, @collection.books.limit(3)] : [200, @quote.collection.books.limit(3)]
+    render :show, status: status
   end
-
 end
