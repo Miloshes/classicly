c329cdb47950d609bcaaa22caae44b634cd98a39
Zsolt Malanyi <zsolt@spreadsong.com>
2013-01-31 21:07:20 +0100

    trying to fix Facebook oauth issue for already registered users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -76,6 +76,8 @@ class ApplicationController < ActionController::Base
   def facebook_cookies
     if session[:facebook_cookies].blank?
       session[:facebook_cookies] = Koala::Facebook::OAuth.new(Facebook::APP_ID, Facebook::SECRET).get_user_info_from_cookies(cookies)
+    rescue Exception
+      return nil
     end
 
     return session[:facebook_cookies]
